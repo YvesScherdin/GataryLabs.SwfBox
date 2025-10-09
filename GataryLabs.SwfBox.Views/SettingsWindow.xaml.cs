e6a2eb91fc0df6d5856506295244ae58d84213bf
Yves Scherdin <yves.scherdin@gmx.de>
2025-10-09 20:53:55 +0200

    +ico +DI packgs

---
+++ GataryLabs.SwfBox.Views/SettingsWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AdonisUI.Controls;
+using System.Windows;
+
+namespace GataryLabs.SwfBox.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : AdonisWindow
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
