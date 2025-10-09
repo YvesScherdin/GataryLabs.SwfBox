bdd895f6ec9f7f4f53799c180e832f87ea1c4d3b
Yves Scherdin <yves.scherdin@gmx.de>
2025-10-09 15:19:28 +0200

    integrate Adonis ui framework

---
+++ GataryLabs.SwfBox.Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AdonisUI.Controls;
+
+namespace GataryLabs.SwfBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AdonisWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
