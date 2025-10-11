9ce3ff2cc428c0918615b19065108b62ccf99a9d
Yves Scherdin <yves.scherdin@gmx.de>
2025-10-11 21:42:00 +0200

    + base controls with mvvm integration

---
+++ GataryLabs.SwfBox.Views/ContentView.cs
@@ -0,0 +1,34 @@
+﻿using GataryLabs.SwfBox.ViewModels.Abstractions;
+using GataryLabs.SwfBox.Views.Abstractions;
+using GataryLabs.SwfBox.Views.InternalBehaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GataryLabs.SwfBox.Views
+{
+    /// <summary>
+    /// Base class for content views.
+    /// </summary>
+    /// <see cref="UserControl"/>
+    public class ContentView : UserControl, IContentView
+    {
+        protected ControlBehaviorManager controlBehaviorManager;
+
+        public IViewModel ViewModel => DataContext as IViewModel;
+
+        public ContentView()
+        {
+            controlBehaviorManager = new ControlBehaviorManager(this);
+
+            this.Unloaded += ContentView_Unloaded;
+        }
+
+        private void ContentView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.Unloaded -= ContentView_Unloaded;
+
+            controlBehaviorManager.Dispose();
+            controlBehaviorManager = null;
+        }
+    }
+}
---
+++ GataryLabs.SwfBox.Views/MvvmWindow.cs
@@ -0,0 +1,28 @@
+﻿using AdonisUI.Controls;
+using GataryLabs.SwfBox.Views.InternalBehaviors;
+using System.Windows;
+
+namespace GataryLabs.SwfBox.Views
+{
+    public class MvvmWindow : AdonisWindow
+    {
+        protected ControlBehaviorManager controlBehaviorManager;
+
+        public MvvmWindow()
+        {
+            controlBehaviorManager = new ControlBehaviorManager(this);
+
+            controlBehaviorManager.Add(new ViewModelOwnerControlBehavior());
+
+            this.Unloaded += MvvmWindowUnloaded;
+        }
+
+        private void MvvmWindowUnloaded(object sender, RoutedEventArgs e)
+        {
+            this.Unloaded -= MvvmWindowUnloaded;
+
+            controlBehaviorManager.Dispose();
+            controlBehaviorManager = null;
+        }
+    }
+}
