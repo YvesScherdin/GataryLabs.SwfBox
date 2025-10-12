d2f932d1bac37d2e611c26978309fb263d3ccf68
Yves Scherdin <yves.scherdin@gmx.de>
2025-10-12 11:38:33 +0200

    upd styles and main content

--- GataryLabs.SwfBox.ViewModels.Abstractions/IMainWindowSwfDetailsContentViewModel.cs
+++ GataryLabs.SwfBox.ViewModels.Abstractions/IMainWindowSwfDetailsContentViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using GataryLabs.SwfBox.ViewModels.Abstractions.DataModels;
+using System.Windows.Input;
 
 namespace GataryLabs.SwfBox.ViewModels.Abstractions
 {
     public interface IMainWindowSwfDetailsContentViewModel : IMainWindowContentViewModel
     {
         ISwfFileDetailsDataModel Details { get; set; }
+
+        ICommand PlayCommand { get; }
+
+        ICommand AnalyzeCommand { get; }
     }
 }
--- GataryLabs.SwfBox.ViewModels/MainWindowSwfDetailsContentViewModel.cs
+++ GataryLabs.SwfBox.ViewModels/MainWindowSwfDetailsContentViewModel.cs
@@ -1,17 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using GataryLabs.SwfBox.ViewModels.Abstractions;
 using GataryLabs.SwfBox.ViewModels.Abstractions.DataModels;
 using GataryLabs.SwfBox.ViewModels.DataModel;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace GataryLabs.SwfBox.ViewModels
 {
     internal class MainWindowSwfDetailsContentViewModel : ObservableObject, IMainWindowSwfDetailsContentViewModel
     {
         private ISwfFileDetailsDataModel details;
+        private RelayCommand playCommand;
+        private RelayCommand analyzeCommand;
 
         public MainWindowSwfDetailsContentViewModel()
         {
-
+            playCommand = new RelayCommand(() => Debug.WriteLine("File is played!"));
+            analyzeCommand = new RelayCommand(() => Debug.WriteLine("File gets analyzed!"));
         }
 
         public ISwfFileDetailsDataModel Details
@@ -20,6 +26,9 @@ namespace GataryLabs.SwfBox.ViewModels
             set => SetProperty(ref details, value);
         }
 
+        public ICommand PlayCommand => playCommand;
+        public ICommand AnalyzeCommand => analyzeCommand;
+        
         public void OnLoaded()
         {
             Details = new SwfFileDetailsDataModel
